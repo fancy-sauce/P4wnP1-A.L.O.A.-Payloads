3468bdcb10c1b24475fb68e1498334705e0f8986
fancy-sauce <60056952+fancy-sauce@users.noreply.github.com>
2025-01-09 09:21:41 -0800

    Update fixpackages.sh
    Changed to python3 package

--- fixpackages.sh
+++ fixpackages.sh
@@ -2,5 +2,5 @@
 wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2024.1_all.deb
 dpkg -i kali-archive-keyring_2024.1_all.deb
 apt update
-apt install python -y
+apt install python3 -y
 rm kali-archive-keyring_2024.1_all.deb
