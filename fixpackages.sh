8218dfb6070f9fa8231d5a904df33534d074c5c2
fancy-sauce <60056952+fancy-sauce@users.noreply.github.com>
2025-01-09 00:32:31 -0800

    Create fixpackages.sh

---
+++ fixpackages.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2024.1_all.deb
+dpkg -i kali-archive-keyring_2024.1_all.deb
+apt install python -y
+rm kali-archive-keyring_2024.1_all.deb
